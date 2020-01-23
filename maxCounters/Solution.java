76f96e8baadd90952dd5b5d7ae747077c448f811
ofietze <oliver.fietze@akamu.de>
2020-01-23 09:06:15 +0100

    Add 100% solution for maxCounters

--- maxCounters/Solution.java
+++ maxCounters/Solution.java
@@ -1,39 +1,41 @@
-// Score 77%, 100% Correctness, 60% Perfomance
+// Task:
 // Return an array of size N of counters
 // If a number X in A is € [1, N] increment counter X
 // If X == N+1 set all counters to the maximum counter value
+// Task Score 100% 
 class Solution {
-    
-    // Time complexity depends on number of calls to maxCounter
-    // o(N), O(N^N)
+    // Time complexity 
+    // max(O(N), O(M))
     public int[] solution(int N, int[] A) {
-        // write your code in Java SE 8
         int[] counters = new int[N];
-        int max = 0;
+        int lastMax = 0;
+        int localMax = 0;
         
+        // O(A.length)
         for (int i = 0; i < A.length; i++) {
             int current = A[i];
-            
+
             if (current >= 1 && current <= N) {
                 counters[current - 1] += 1;
-                
+
                 // keep track of max counter value
-                if (counters[current - 1] > max) {
-                    max = counters[current - 1];
+                if (counters[current - 1] > localMax) {
+                    localMax = counters[current - 1];
                 }
             } else if (current == N + 1) {
-               counters = maxCounter(max, counters);
+            // The idea is to reset the counters and only keep track 
+            // of the changes from here on. We later add the max again.    
+               lastMax += localMax;  // Add to last max to counteract the reset
+               if (localMax != 0) counters = new int[N]; // reset array only when necessary
+               localMax = 0;         // reset localMax
+               
             }
         }
-        return counters;
-    }
-    
-    // Set all values in arr to max
-    // Time complexity O(arr.length)
-    private int[] maxCounter(int max, int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = max;
+       
+       // O(N)
+        for (int i = 0; i < N; i++) {
+            counters[i] += lastMax;
         }
-        return arr;
+        return counters;
     }
 }
